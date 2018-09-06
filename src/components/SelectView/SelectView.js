87c8c5c1fa9658705de861943b3700788c39fd9b
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-06 14:47:15 -0500

    gets pizzas from server on component mount

--- src/components/SelectView/SelectView.js
+++ src/components/SelectView/SelectView.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 class SelectView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pizzas: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/api/pizza')
+    .then(response => {
+      console.log(response.data);
+      
+    }).catch(error => {
+      console.log('/api/pizza GET error:', error);
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Step 1: Select Your Pizza</h1>
+        <ul>
+
+        </ul>
       </div>
     );
   }
