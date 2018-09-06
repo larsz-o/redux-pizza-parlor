fcbca8ea1025281b404f316083e8ea05fb52b75e
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-06 13:43:06 -0500

    Setup redux reducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,64 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = (state = [], action) => {
+  if (action.type === 'GET_HISTORY') {
+    const orderHistoryFromServer = action.payload;
+    return orderHistoryFromServer;
+  }
+  return state;
+};
+
+const blankOrder = {
+  customer: { 
+      name: "",
+      street_address: "",
+      city: "",
+      zip: null
+  },
+  pizzas: [], // pizza objects go here
+  order_total: 0,
+  type: "Pickup" // Pickup and Delivery
+};
+
+const currentOrder = (state = blankOrder, action) => {
+  // DO NOT MUTATE STATE!!!
+  // Changes Pizza only 
+  if (action.type === 'ADD_PIZZA') {
+    const newPizzas = [...state.pizzas, action.payload];
+    const newState = {
+      ...state,
+      pizzas: newPizzas
+    };
+    return newState; // this becomes our "state" the next time the reducer is run
+  } // changes customer info
+    else if (action.type === 'ADD_INFO') {
+    const newCustomer = action.payload;
+    const newState = {
+      ...state,
+      customer: newCustomer
+    };
+    return newState;
+  }
+    // Sets state back to blank order
+    else if (action.type === 'CLEAR_ORDER') {
+      return blankOrder;
+    }
+  return state;
+};
+
+const storeInstance = createStore(
+  combineReducers({
+    history,
+    currentOrder
+  }),
+  applyMiddleware(logger),
+ );
+ 
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
