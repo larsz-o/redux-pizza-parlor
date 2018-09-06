e220c83509cabdf8fa1cdd3e02435e4b4ff07cee
Isaac Overlander <Isaac.overlander@gmail.com>
2018-09-06 14:27:26 -0500

    Set up CustomerView Component

---
+++ src/components/CustomerView/CustomerView.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class CustomerView extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default connect()(CustomerView);
